0379df0dd1707b77b0b7e5e32dc947f7eeaf7b5b
kriss <kriss@xixs.com>
2023-03-31 01:04:03 +0100

    download base text for processing

---
+++ chambers/download.sh
@@ -0,0 +1,8 @@
+
+echo " downloading the Chambers's Twentieth Century Dictionary "
+
+curl -L "https://www.gutenberg.org/ebooks/37683.txt.utf-8" -o dict-abcd.txt
+curl -L "https://www.gutenberg.org/ebooks/38538.txt.utf-8" -o dict-efghijklm.txt
+curl -L "https://www.gutenberg.org/ebooks/38699.txt.utf-8" -o dict-nopqr.txt
+curl -L "https://www.gutenberg.org/ebooks/38700.txt.utf-8" -o dict-stuvwxyz.txt
+
