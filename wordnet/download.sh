b6fd8beb0539105975ad87e1de20145f25305bf2
kriss <kriss@xixs.com>
2023-05-17 14:23:57 +0100

    .

--- wordnet/download.sh
+++ wordnet/download.sh
@@ -5,3 +5,4 @@ curl -L "https://wordnetcode.princeton.edu/wn3.1.dict.tar.gz" -o dict.tar.gz
 tar -zxvf dict.tar.gz
 rm dict.tar.gz
 
+# format documentation can be found in section 5 of https://wordnet.princeton.edu/documentation
